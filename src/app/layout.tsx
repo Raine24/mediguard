6c58a8f3bdca67d1bd262efd62e2e48adc5e9a58
Raine24 <raine24@users.noreply.github.com>
2026-07-30 18:01:21 +0300

    chore: add live Clerk Publishable and Secret Keys to project and fallback configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <ClerkProvider>
+        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY || "pk_live_Y2xlcmsubWVkaWNpbnRpbWUuY29tJA"}>
           {children}
           <CookieConsent />
         </ClerkProvider>
